c0c57773a31b43027ecf0171c1717333301bfe0c
Radwa Khalil <95037451+radwakhalil22@users.noreply.github.com>
2023-05-15 13:27:12 +0300

    lab1

---
+++ lab1/src/App.js
@@ -0,0 +1,14 @@
+import ElementsClass from './Components/Slider/slider';
+import FormClass from './Components/form/form';
+
+
+function App() {
+    return (
+      <div>
+        <ElementsClass />
+        <FormClass/>
+      </div>
+    );
+}
+
+export default App;
